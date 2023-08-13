9cb0c0aeffafa1ee2e4025990b69594ce8eebf6a
prandogabriel <gprando55@gmail.com>
2023-08-12 22:06:27 -0300

    feat: nodejs

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,30 @@
+const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const client = new DynamoDBClient({});
+
 module.exports.handler = async (event) => {
+  const id = event.pathParameters.id ?? "";
+
+  const result = await client.send(
+    new GetItemCommand({ TableName: "User", Key: { id: { S: id } } })
+  );
+
+  if (!result.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify(
+        {
+          error: "not found user",
+        },
+        null,
+        2
+      ),
+    };
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(unmarshall(result.Item ?? {})),
   };
 };
