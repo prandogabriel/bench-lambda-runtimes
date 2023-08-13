d515e34c0ba2b298905a0b417a41c3ac434d0c3d
prandogabriel <gprando55@gmail.com>
2023-08-13 00:03:43 -0300

    feat: add python code

--- python/handler.py
+++ python/handler.py
@@ -1,12 +1,41 @@
 import json
+import boto3
+from boto3.dynamodb.types import TypeSerializer, TypeDeserializer
+
+td = TypeDeserializer()
+
+dynamodb = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    try:
+        user_id = event['pathParameters']['id']
+        
+        response = dynamodb.get_item(
+            TableName='User',
+            Key={
+                'id': {'S': user_id}
+            }
+        )
+        
+        if 'Item' in response:
+            user = response['Item']
+            return {
+                'statusCode': 200,
+                'body': json.dumps(unmarshall(user))
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': json.dumps('{error: "not found user"}')
+            }
+            
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
-    return response
+def unmarshall(dynamo_obj: dict) -> dict:
+    """Convert a DynamoDB dict into a standard dict."""
+    return {k: td.deserialize(v) for k, v in dynamo_obj.items()}
